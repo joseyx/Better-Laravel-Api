1426c251e411a7a568a22afad8f478b499c21f66
Jose Rojas <joseyx041199@gmail.com>
2023-12-11 01:57:12 -0400

    Added dashboard routes group and get users endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::get('users', [UserController::class, 'index']);
 });
 
 Route::group(['prefix' => 'auth'], function () {
